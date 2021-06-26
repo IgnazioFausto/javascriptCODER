915a87bd50875a757080c3a076b54cdc511cf9c2
ignaciopollito <nachojam777@gmail.com>
2021-06-26 16:45:56 -0300

    primer ejercicio en js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-var nombre;
-nombre = 'salcha';
-console.log (nombre);
+let nombreIngresado = prompt("Ingresá tu nombre");
+let apellidoIngresado = prompt("Ingresá tu apellido");
+
+if ((nombreIngresado !="") && (apellidoIngresado !="")) {
+    alert ("¡Hola y bienvenido " +nombreIngresado +" " +apellidoIngresado +"!")
+}
+else {
+    alert("Datos incompletos")
+}
