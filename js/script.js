dfb852d811f0507e5a45575ba1faffc18e1faeea
ignaciopollito <nachojam777@gmail.com>
2021-07-19 23:04:22 -0300

    agregando objetos y ordenando con array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/* construyendo */
 function Cuadro (titulo, medidas, precio) {
    this.titulo = titulo;
    this.medidas = medidas;
@@ -11,17 +11,40 @@ const Vuelo = new Cuadro ("Vuelo", "45cm X 68cm", 2500)
 const Pareja = new Cuadro ("Pareja", "75cm X 75cm", 2900)
 const Pájaro = new Cuadro ("Pájaro", "45cm X 45cm", 1500)
 
+/* array con todos los cuadros */
 const arrayCuadros = [Perro, Zorro, Mar, Vuelo, Pareja, Pájaro];
-   
-/* const baratos = arrayCuadros.filter(Cuadro => Cuadro.precio < 3000);  */
 
 /* pidiendo información al usuario */
 let CuadroElegido = prompt("Ingresá el nombre del cuadro (Perro, Zorro, Mar, Vuelo, Pareja o Pájaro)")
 var PrecioFinal = 0;
+
+/* informar que se aplicará el descuento */
 alert("Se aplicará el 10% de descuento")
 
 /* encontrando el cuadro elegido */
-/* HACER CON SWITCH LPM */
+if (CuadroElegido === Perro.titulo) {
+   alert("El precio final del cuadro es: $ " + (Perro.precio - (Perro.precio * 10 / 100)))
+}  
+   else if (CuadroElegido === Zorro.titulo) {
+      alert("El precio final del cuadro es: $ " + (Zorro.precio - (Zorro.precio * 10 / 100)))
+   }  
+   else if (CuadroElegido === Mar.titulo) {
+      alert("El precio final del cuadro es: $ " + (Mar.precio - (Mar.precio * 10 / 100)))
+   }
+   else if (CuadroElegido === Vuelo.titulo) {
+      alert("El precio final del cuadro es: $ " + (Vuelo.precio - (Vuelo.precio * 10 / 100)))
+   }
+   else if (CuadroElegido === Pareja.titulo) {
+      alert("El precio final del cuadro es: $ " + (Pareja.precio - (Pareja.precio * 10 / 100)))
+   }else if (CuadroElegido === Pájaro.titulo) {
+      alert("El precio final del cuadro es: $ " + (Pájaro.precio - (Pájaro.precio * 10 / 100)))
+   }
+   else {
+      alert("No has elegido un cuadro.")
+   }
+   
 
 
+   /* para filtrar por precios más adelante */
+   /* const baratos = arrayCuadros.filter(Cuadro => Cuadro.precio < 3000);  */
 
