ca5afcaac6d8bc4b76a3124487bc71ae2617f27f
ignaciopollito <nachojam777@gmail.com>
2021-07-21 23:56:56 -0300

    quitando if else, tengo que solucionar con function

--- js/script.js
+++ js/script.js
@@ -4,44 +4,45 @@ function Cuadro (titulo, medidas, precio) {
    this.medidas = medidas;
    this.precio = precio;
 }
-const Perro = new Cuadro ("Perro", "75cm X 98cm", 7500)
-const Zorro = new Cuadro ("Zorro", "85cm X 99cm", 6500)
-const Mar = new Cuadro ("Mar", "125cm X 90cm", 5700)
-const Vuelo = new Cuadro ("Vuelo", "45cm X 68cm", 2500)
-const Pareja = new Cuadro ("Pareja", "75cm X 75cm", 2900)
-const Pájaro = new Cuadro ("Pájaro", "45cm X 45cm", 1500)
+const perro = new Cuadro ("Perro", "75cm X 98cm", 7500)
+const zorro = new Cuadro ("Zorro", "85cm X 99cm", 6500)
+const mar = new Cuadro ("Mar", "125cm X 90cm", 5700)
+const vuelo = new Cuadro ("Vuelo", "45cm X 68cm", 2500)
+const pareja = new Cuadro ("Pareja", "75cm X 75cm", 2900)
+const pájaro = new Cuadro ("Pájaro", "45cm X 45cm", 1500)
 
 /* array con todos los cuadros */
-const arrayCuadros = [Perro, Zorro, Mar, Vuelo, Pareja, Pájaro];
+const arrayCuadros = [perro, zorro, mar, vuelo, pareja, pájaro];
 
 /* pidiendo información al usuario */
-let CuadroElegido = prompt("Ingresá el nombre del cuadro (Perro, Zorro, Mar, Vuelo, Pareja o Pájaro)")
-var PrecioFinal = 0;
-
+let cuadroElegido = prompt("Ingresá el nombre del cuadro (Perro, Zorro, Mar, Vuelo, Pareja o Pájaro)");
+/* let precioFinal = 0;*/
 /* informar que se aplicará el descuento */
 alert("Se aplicará el 10% de descuento")
 
 /* encontrando el cuadro elegido */
-if (CuadroElegido === Perro.titulo) {
-   alert("El precio final del cuadro es: $ " + (Perro.precio - (Perro.precio * 10 / 100)))
+/* if (CuadroElegido === perro.titulo) {
+   alert("El precio final del cuadro es: $ " + (perro.precio - (perro.precio * 10 / 100)))
 }  
-   else if (CuadroElegido === Zorro.titulo) {
-      alert("El precio final del cuadro es: $ " + (Zorro.precio - (Zorro.precio * 10 / 100)))
+   else if (CuadroElegido === zorro.titulo) {
+      alert("El precio final del cuadro es: $ " + (zorro.precio - (zorro.precio * 10 / 100)))
    }  
-   else if (CuadroElegido === Mar.titulo) {
-      alert("El precio final del cuadro es: $ " + (Mar.precio - (Mar.precio * 10 / 100)))
+   else if (CuadroElegido === mar.titulo) {
+      alert("El precio final del cuadro es: $ " + (mar.precio - (mar.precio * 10 / 100)))
    }
-   else if (CuadroElegido === Vuelo.titulo) {
-      alert("El precio final del cuadro es: $ " + (Vuelo.precio - (Vuelo.precio * 10 / 100)))
+   else if (CuadroElegido === vuelo.titulo) {
+      alert("El precio final del cuadro es: $ " + (vuelo.precio - (vuelo.precio * 10 / 100)))
    }
-   else if (CuadroElegido === Pareja.titulo) {
-      alert("El precio final del cuadro es: $ " + (Pareja.precio - (Pareja.precio * 10 / 100)))
-   }else if (CuadroElegido === Pájaro.titulo) {
-      alert("El precio final del cuadro es: $ " + (Pájaro.precio - (Pájaro.precio * 10 / 100)))
+   else if (CuadroElegido === pareja.titulo) {
+      alert("El precio final del cuadro es: $ " + (pareja.precio - (pareja.precio * 10 / 100)))
+   }else if (CuadroElegido === pájaro.titulo) {
+      alert("El precio final del cuadro es: $ " + (pájaro.precio - (pájaro.precio * 10 / 100)))
    }
    else {
       alert("No has elegido un cuadro.")
-   }
+   } */
+  
+
    
 
 
