3536afe5798065ceb163973f3f6363aae456bae4
ignaciopollito <nachojam777@gmail.com>
2021-07-26 00:05:08 -0300

    mejorando diseño y agregando appendchild

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 
 var nombre = prompt("Hola, escribí tu nombre!");
-localStorage.setItem("nombre",nombre);
-
+/* localStorage.setItem("nombre",nombre); */
+/* 
 let parrafo = document.createElement("p");
 // Insertar HTML interno
-parrafo.innerHTML= "¡Bienvenido " + nombre + "!"; 
+var texto = document.createTextNode("¡Bienvenido " + nombre + "! Espero sea de tu agrado mi obra.");
+div.appendChild(texto) ;
 // Añadir el nodo Element como hijo de body
-document.body.appendChild(parrafo);
-
+ */
+var para = document.createElement("P");                       // Create a <p> node
+var t = document.createTextNode("¡Hola " + nombre + "! Espero te agrade mi obra.");      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("saludo").appendChild(para);           // Append <p> to <div> with id="myDIV"
 /* construyendo 
 function Cuadro (titulo, medidas, precio) {
    this.titulo = titulo;
